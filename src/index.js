4ee663a61a860f9f638ba661d076fcee457a906a
hbhatt18 <het.bhatt.ce.18@gmail.com>
2020-10-22 01:38:31 -0400

    Code Refactoring and Cleanup

--- src/index.js
+++ src/index.js
@@ -1,132 +1,128 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
-import { render } from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-import * as serviceWorker from './serviceWorker'
-import './App.css';
-import $ from 'jquery'
-import Rx from 'rxjs/Rx'
-import DataTable from 'react-data-table-component';
-import BootstrapTable from 'react-bootstrap-table-next';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import './App.css'
+import BootstrapTable from 'react-bootstrap-table-next'
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter'
 
-
-const timeObservable = Rx.Observable.interval(9000);
-export const mock = Rx.Observable.create((ob) => {
-	timeObservable.subscribe(() => {
-	  Rx.Observable.from(assets)
-		.map(asset => {
-		  const random = Math.random();
-		  asset.price = random >= 0.5 ? asset.price + random : asset.price - random;
-        asset.lastUpdate = Date.now();
-		  return asset;
-		})
-		.subscribe(asset => ob.next(asset));
-	});
-	return () => null; // we don't care about unsubscribe just for a test
-});
-
+// create the assets
+// here the name appends with id just to create unique asset name
 const createAsset = (id, type) => {
-   return {
-     id: id,
-     assetName: type === 'Stock' ? ['AAPL','GOOGL','FB', 'TSLA', 'MSFT'][Math.floor(Math.random() * 4)] : ['EUR','USD','GBP', 'NIS', 'AUD'][Math.floor(Math.random() * 4)],
-     price: Math.random()*10,
-     lastUpdate: Date.now(),
-     type: type
-   }
- };
- 
- const getAllAssets = (n) => {
-   const result = [];
-   for (let i = 0; i < n; i++) {
-     result.push(createAsset(i, 'Stock'));
-     result.push(createAsset(i+n, 'Currency'));
-   }
-   return result;
- }
- 
-const assets = getAllAssets(200);
+  return {
+    id: id,
+    assetName: type === 'Stock' ? ['AAPL' + id, 'GOOGL' + id, 'FB' + id, 'TSLA' + id, 'MSFT' + id][Math.floor(Math.random() * 4)] : ['EUR' + id, 'USD' + id, 'GBP' + id, 'NIS' + id, 'AUD' + id][Math.floor(Math.random() * 4)],
+    price: Math.random() * 10,
+    lastUpdate: Date.now(),
+    type: type
+  }
+}
+
+const getAllAssets = (n) => {
+  const result = []
+  // i starts from 1 because id starts from 1
+  // call createAsset function to create 200 Stocks and 200 Currencies to make 400 assets
+  for (let i = 1; i <= n; i++) {
+    result.push(createAsset(i, 'Stock'))
+    result.push(createAsset(i + n, 'Currency'))
+  }
+  return result
+}
+
+const headerSortingStyle = { backgroundColor: 'lightgray' };
 
 const columns = [
   {
     dataField: 'id',
     text: 'ID',
     sort: true,
+    headerSortingStyle,
     filter: textFilter()
   },
   {
     dataField: 'assetName',
     text: 'Asset Name',
     sort: true,
+    headerSortingStyle,
     filter: textFilter()
   },
   {
     dataField: 'price',
     text: 'Price',
     sort: true,
+    headerSortingStyle,
     filter: textFilter()
   },
   {
     dataField: 'lastUpdate',
     text: 'Last Updated',
     sort: true,
+    headerSortingStyle,
     filter: textFilter()
   },
   {
     dataField: 'type',
     text: 'Type',
     sort: true,
+    headerSortingStyle,
     filter: textFilter()
   }
-];
+]
 
 const defaultSorted = [{
   dataField: 'price',
   order: 'desc'
-}];
+}]
 
 class Table extends React.Component {
-  constructor(props) {
-     super(props)
-     this.state = {
-        assets: getAllAssets(200)
-     }
+  constructor (props) {
+    super(props)
+    this.state = {
+      assets: getAllAssets(200)
+    }
   }
 
- componentDidMount() {
-   const mock = Rx.Observable.create((ob) => {
-     timeObservable.subscribe(() => {
-       Rx.Observable.from(this.state.assets).map((asset) => {
-         const random = Math.random();
-         asset.price = random >= 0.5 ? asset.price + random : asset.price - random;
-         return asset;
-       }).subscribe((asset) => ob.next(asset));
-     });
-     return () => null;
-   });
-   const p = mock.subscribe((asset) => {
-      asset.price = asset.price;
-    });
- }
+  mockRefreshData () {
+    const random = Math.random()
 
-  render(){
-    return(
+    // map through the assets and store it in newAssets to return the data that is changed
+    const newAssets = this.state.assets.map((asset) => {
+      // we are either increasing the price or decreasing it
+      // so based on random number the price will change from -1 to 1
+      const newPrice = random >= 0.5 ? asset.price + random : asset.price - random
+      const newLastUpdate = Date.now()
+      return { ...asset, price: newPrice, lastUpdate: newLastUpdate }
+    })
+
+    // call setState method to provide new value so React will call render() method to reflect the changes
+    this.setState({ assets: newAssets })
+  }
+
+  componentDidMount () {
+    // change value of asset price every second
+    this.timerID = setInterval(() => this.mockRefreshData(), 1000)
+  }
+
+  componentWillUnmount () {
+    // need to use the unmount lifecycle method to stop the timer.
+    clearInterval(this.timerID)
+  }
+
+  render () {
+    return (
       <div>
-      <h1 id='title'>Assets Realtime Details</h1>
-    <BootstrapTable
-      keyField="id"
-      data={ this.state.assets }
-      columns={ columns }
-      defaultSorted={ defaultSorted }
-      filter={ filterFactory() }
-    />
-    </div>
+        <h1 id='title'>Assets Realtime Details</h1>
+        <BootstrapTable
+          bootstrap4
+          keyField="id"
+          data={ this.state.assets }
+          columns={ columns }
+          defaultSorted={ defaultSorted }
+          filter={ filterFactory() }
+        />
+      </div>
     )
-    
   }
 }
 
-ReactDOM.render(<Table />, document.getElementById('root'));
-
-serviceWorker.unregister()
+ReactDOM.render(<Table />, document.getElementById('root'))
